679947697c5b89c9772702718dd0b5980f770c77
tantaitanluoc <tantaitanluoc@gmail.com>
2018-09-05 23:27:47 +0700

    them linh tinh, gan xong

--- randomFiles.py
+++ randomFiles.py
@@ -3,7 +3,8 @@ import anydbm
 import pickle
 import random
 
-db = anydbm.open("files_browsed.db","c")
+db_name = "files_browsed.db"
+db = anydbm.open(db_name,"c")
 def randomchoose(names,file_block):
 	# choose randomly [file_block] files from a file list
 	chossen = []
@@ -14,24 +15,34 @@ def randomchoose(names,file_block):
 			flag = False
 		else:
 			temp = random.choice(names)
+			hashed_value = bitch.md5_checksum(temp)
+			# store in md5 value so don't worry about the same file but with different names
 			if temp not in db:
-				chossen.append(temp)
+				chossen.append(hashed_value) 
 				count += 1
-			else:
-				print "Duplication found in ",temp
 	return chossen
 
+def copyfile(file,destination):
+	cmd = "cp " + file+ " "+destination
+	return bitch.pipe(cmd)
 
-def scan(dirname="",file_block=5):
+def reset_database():
+	cmd = "mv " +db_name+" "+db_name+".bak" # backup database
+	print "Your database is being reset..."
+	return bitch.pipe(cmd)
+
+def scan(dirname="",file_block=5,destination="/home/tantai/ao"):
 	names = bitch.walk(dirname)
 	chossen = randomchoose(names,file_block)
 	for file in chossen:
 		db[file] = bitch.get_time()
+		copyfile(file,destination)
 	print "Done!"
 
 
 if __name__ == '__main__':
-	scan("/run/media/tantai/DATA/py_test/review/")
+	# scan("/run/media/tantai/DATA/py_test/review/")
+	reset_database()
 
 
 
